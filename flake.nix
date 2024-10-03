24954d9671ae01c2dbef211389f090a6818ad411
Mathieu Moalic <matmoa@pm.me>
2024-10-03 10:33:15 +0200

    Add flakes

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fix-python.url = "github:GuillaumeDesforges/fix-python";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    fix-python,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        devEnv = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            fix-python.packages.${system}.default
+            python311
+            ruff
+            ruff-lsp
+            ffmpeg
+          ];
+
+          shellHook = ''
+            set -euo pipefail
+            test -d .venv || (${pkgs.python3.interpreter} -m venv .venv && source .venv/bin/activate && pip install -r requirements.txt && fix-python --venv .venv && echo "use flake" > .envrc && direnv allow)
+            source .venv/bin/activate
+          '';
+        };
+      in {
+        devShell = devEnv;
+      }
+    );
+}
